1eb069f2c16be05f354ee39275afd4fa8c5967a2
palmdog57 <tompalmer12@gmail.com>
2020-01-14 23:08:20 +0000

    Added ability to loop through categories

--- palmersTools.js
+++ palmersTools.js
@@ -1,17 +1,10 @@
 function getCategories() {
-  chrome.storage.sync.get('categories', function callback(items) { 
-    console.log("BEFORE", items)
+  chrome.storage.sync.get('responses', function callback(items) { 
     Object.keys(items).forEach((entry) =>
     {
       let currentKey = entry;
       let currentValue = items[entry];
-
-      console.log(typeof(currentKey));
-      console.log(typeof(currentValue));
-
-      currentValue.push("Hola");
-      console.log("AFTER", currentValue)
-
+      console.log(currentValue);
     });
   });
 }
@@ -20,50 +13,67 @@ function getCategories() {
 function save_category() {
     console.log("Saving category to local storage");
     var categoryTitle = document.getElementById('categoryTitle').value;
-    var btnType = document.getElementById('btnCat').value;
 
-    var buttonObject = {
-        "type" : btnType,
-        "categoryTitle" : categoryTitle
-    }
+    chrome.storage.sync.get('categories', function callback(items) { 
+      Object.keys(items).forEach((entry) => {
+        let currentKey = entry;
+        let currentValue = items[entry];
+  
+        currentValue.push(categoryTitle);
+        console.log(currentValue);
 
-    chrome.storage.sync.set({
-        [categoryTitle]: buttonObject
-    }, function() {
-      // Update status to let user know options were saved.
-      var status = document.getElementById('status');
-      status.textContent = 'Category added successfully.';
-      
-      setTimeout(function() {
-        status.textContent = '';
-      }, 750);
-    });
+        chrome.storage.sync.set({
+          categories: currentValue
+      }, function() {
+        // Update status to let user know options were saved.
+        var status = document.getElementById('status');
+        status.textContent = 'Category added successfully.';
+        
+        setTimeout(function() {
+          status.textContent = '';
+        }, 750);
+      }); //END STORAGE SET
+  
+      }); //END OBJECT KEYS
+    }); //END STORAGE GET
+
+
 }
 
 // Save responses to chrome.storage
 function save_responses() {
-    console.log("Saving response to local storage");
+    //console.log("Saving response to local storage");
     var resName = document.getElementById('resName').value;
     var resBody = document.getElementById('resBody').value;
-    var btnType = document.getElementById('btnRes').value;
     
-    var responseObject = {
-        "type" : btnType,
-        "resName" : resName,
-        "resBody" : resBody
-    }
+    chrome.storage.sync.get('responses', function callback(items) { 
+      Object.keys(items).forEach((entry) => {
+        let currentKey = entry;
+        let currentValue = items[entry];
+
+        var fuckYou = JSON.parse(items);
+        console.log(typeof(fuckYou));
+
+        var responseObject = {resName : resBody}
+  
+        currentValue.push(responseObject);
     
-    chrome.storage.sync.set({
-        [resName]: responseObject
-    }, function() {
-      // Update status to let user know options were saved.
-      var status = document.getElementById('status');
-      status.textContent = 'Response added successfully.';
-      
-      setTimeout(function() {
-        status.textContent = '';
-      }, 750);
-    });
+        chrome.storage.sync.set({
+          responses: responseObject
+        }, function() {
+          // Update status to let user know options were saved.
+          var status = document.getElementById('status');
+          status.textContent = 'Response added successfully.';
+          
+          setTimeout(function() {
+            status.textContent = '';
+          }, 750);
+        }); //END STORAGE SET
+  
+      }); //END OBJECT KEYS
+    }); //END STORAGE GET
+
+
 }
 
 function loadOptions(){
